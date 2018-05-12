c6e262f69bb7d0437efd7775787cfc80c2981d93
Kino  Rose <kinoarose@gmail.com>
2018-05-12 18:51:05 -0400

    Add side project information to navigation links

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,21 @@
 <template>
-  <div id="main">
+  <div id="main" class="container">
     <div class="row">
       <div class="col-12">
-        Buttons
+        <div class="col-6">
+          <ul class="nav nav-pills">
+
+          </ul>
+        </div>
+        <div class="col-6">
+          <ul class="nav nav-pills">
+
+          </ul>
+        </div>
       </div>
       <div class="col-12">
-        <div class="row">
-          <div class="col-8">
+        <div class="row main-area">
+          <div class="col-8 main-content">
             <div class="card">
               <div class="card-header">
                 Main Content Area
@@ -18,7 +27,7 @@
               </div>
             </div>
           </div>
-        <div class="col-4"> 
+        <div class="col-4 price-content"> 
           <div class="card">
           <div class="card-header"> Pricing </div>
           <div class="card-body">
@@ -43,6 +52,20 @@ export default {
   name: "Main",
   data() {
     return {
+      leftNav: [
+        {
+          title: "About Me", link:"/",
+        },
+        {
+          title:"Work Examples", link:"/workExamples", 
+        },
+        {
+          title: "Side Projects", link:"/sideProjects"
+        }
+      ],
+      rightNav: [
+        {title:"Contact Me", link: ""},
+      ],
       prices: [
         {
           title: "Debugging", price: "15$"
@@ -67,7 +90,19 @@ export default {
 
 
 <style>
+.main-area {
+  border-top: 1px solid #ff696e;
+}
+.main-content {
+  padding-right: 0px !important;
+}
+
+.price-content {
+  padding-left: 0px !important;
+  border-left: 1px solid #ff696e;
+}
 .price-list {
+  padding-left: 0px;
   list-style-type: none;
 }
 </style>
